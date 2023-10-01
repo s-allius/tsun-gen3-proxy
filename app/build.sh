ff9360d2a74f7bcd3abc97fca52e3d17e88ea9cd
Stefan Allius <stefan.allius@t-online.de>
2023-10-01 21:30:40 +0200

    add versioning, build 'rel' and 'dev' variants

--- app/build.sh
+++ app/build.sh
@@ -1,13 +1,31 @@
-#!/bin/sh
+#!/bin/bash
+
+set -e
 
 BUILD_DATE=$(date -Iminutes)
 VERSION=$(git describe --tags --abbrev=0)
 VERSION="${VERSION:1}"
 arr=(${VERSION//./ })
 MAJOR=${arr[0]}
-echo version: $VERSION  build-date: $BUILD_DATE
-exec docker build --label "org.label-schema.build-date=${BUILD_DATE}" --label "org.opencontainers.image.version=${VERSION}" \
- -t "ghcr.io/s-allius/tsun-gen3-proxy:latest" -t "ghcr.io/s-allius/tsun-gen3-proxy:${MAJOR}" -t "ghcr.io/s-allius/tsun-gen3-proxy:${VERSION}" app
+IMAGE=tsun-gen3-proxy
+
+if [[ $1 == dev ]];then
+IMAGE=docker.io/sallius/${IMAGE}
+VERSION=${VERSION}-dev
+elif [[ $1 == rel ]];then
+IMAGE=ghcr.io/s-allius/${IMAGE}
+else
+echo argument missing!
+echo try: $0 '[dev|rel]'
+exit 1
+fi
+
+echo version: $VERSION  build-date: $BUILD_DATE   image: $IMAGE
+if [[ $1 == dev ]];then
+docker build --build-arg "VERSION=${VERSION}" --label "org.label-schema.build-date=${BUILD_DATE}" --label "org.opencontainers.image.version=${VERSION}" -t ${IMAGE}:latest app
+elif [[ $1 == rel ]];then
+docker build --no-cache --build-arg "VERSION=${VERSION}" --label "org.label-schema.build-date=${BUILD_DATE}" --label "org.opencontainers.image.version=${VERSION}" -t ${IMAGE}:latest -t ${IMAGE}:${MAJOR} -t ${IMAGE}:${VERSION} app
 docker push ghcr.io/s-allius/tsun-gen3-proxy:latest
 docker push ghcr.io/s-allius/tsun-gen3-proxy:${MAJOR}
 docker push ghcr.io/s-allius/tsun-gen3-proxy:${VERSION}
+fi
