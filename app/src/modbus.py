54d2bf4439eb5b9ae9e5df12397cfdd520ae560f
Stefan Allius <stefan.allius@t-online.de>
2024-05-07 17:52:51 +0200

    set err value for unit tests

--- app/src/modbus.py
+++ app/src/modbus.py
@@ -32,12 +32,13 @@ class Modbus():
     __crc_tab = []
     map = {
         0x2007: {'reg': Register.MAX_DESIGNED_POWER,   'fmt': '!H', 'ratio':  1},  # noqa: E501
-        0x420100c0: {'reg': Register.INVERTER_STATUS,      'fmt': '!H'},                 # noqa: E501
+        # 0x????: {'reg': Register.INVERTER_STATUS,      'fmt': '!H'},                 # noqa: E501
         0x3008: {'reg': Register.VERSION,              'fmt': '!H', 'eval': "f'v{(result>>12)}.{(result>>8)&0xf}.{(result>>4)&0xf}{result&0xf}'"},  # noqa: E501
         0x3009: {'reg': Register.GRID_VOLTAGE,         'fmt': '!H', 'ratio':  0.1},  # noqa: E501
         0x300a: {'reg': Register.GRID_CURRENT,         'fmt': '!H', 'ratio': 0.01},  # noqa: E501
         0x300b: {'reg': Register.GRID_FREQUENCY,       'fmt': '!H', 'ratio': 0.01},  # noqa: E501
         0x300c: {'reg': Register.INVERTER_TEMP,        'fmt': '!H', 'eval': 'result-40'},  # noqa: E501
+        # 0x300d
         0x300e: {'reg': Register.RATED_POWER,          'fmt': '!H', 'ratio':    1},  # noqa: E501
         0x300f: {'reg': Register.OUTPUT_POWER,         'fmt': '!H', 'ratio':  0.1},  # noqa: E501
         0x3010: {'reg': Register.PV1_VOLTAGE,          'fmt': '!H', 'ratio':  0.1},  # noqa: E501
@@ -53,15 +54,15 @@ class Modbus():
         0x301a: {'reg': Register.PV4_CURRENT,          'fmt': '!H', 'ratio': 0.01},  # noqa: E501
         0x301b: {'reg': Register.PV4_POWER,            'fmt': '!H', 'ratio':  0.1},  # noqa: E501
         0x301c: {'reg': Register.DAILY_GENERATION,     'fmt': '!H', 'ratio': 0.01},  # noqa: E501
-        0x420100fa: {'reg': Register.TOTAL_GENERATION,     'fmt': '!L', 'ratio': 0.01},  # noqa: E501
+        # 0x301d: {'reg': Register.TOTAL_GENERATION,     'fmt': '!L', 'ratio': 0.01},  # noqa: E501
         0x301f: {'reg': Register.PV1_DAILY_GENERATION, 'fmt': '!H', 'ratio': 0.01},  # noqa: E501
-        0x42010100: {'reg': Register.PV1_TOTAL_GENERATION, 'fmt': '!L', 'ratio': 0.01},  # noqa: E501
+        # 0x3020: {'reg': Register.PV1_TOTAL_GENERATION, 'fmt': '!L', 'ratio': 0.01},  # noqa: E501
         0x3022: {'reg': Register.PV2_DAILY_GENERATION, 'fmt': '!H', 'ratio': 0.01},  # noqa: E501
-        0x42010106: {'reg': Register.PV2_TOTAL_GENERATION, 'fmt': '!L', 'ratio': 0.01},  # noqa: E501
+        # 0x3023: {'reg': Register.PV2_TOTAL_GENERATION, 'fmt': '!L', 'ratio': 0.01},  # noqa: E501
         0x3025: {'reg': Register.PV3_DAILY_GENERATION, 'fmt': '!H', 'ratio': 0.01},  # noqa: E501
-        0x4201010c: {'reg': Register.PV3_TOTAL_GENERATION, 'fmt': '!L', 'ratio': 0.01},  # noqa: E501
+        # 0x3026: {'reg': Register.PV3_TOTAL_GENERATION, 'fmt': '!L', 'ratio': 0.01},  # noqa: E501
         0x3028: {'reg': Register.PV4_DAILY_GENERATION, 'fmt': '!H', 'ratio': 0.01},  # noqa: E501
-        0x42010112: {'reg': Register.PV4_TOTAL_GENERATION, 'fmt': '!L', 'ratio': 0.01},  # noqa: E501
+        # 0x3029: {'reg': Register.PV4_TOTAL_GENERATION, 'fmt': '!L', 'ratio': 0.01},  # noqa: E501
     }
 
     def __init__(self):
@@ -70,6 +71,7 @@ class Modbus():
         self.last_fcode = 0
         self.last_len = 0
         self.last_reg = 0
+        self.err = 0
 
     def build_msg(self, addr, func, reg, val):
         msg = struct.pack('>BBHH', addr, func, reg, val)
@@ -77,38 +79,47 @@ class Modbus():
         self.last_fcode = func
         self.last_reg = reg
         self.last_len = val
+        self.err = 0
         return msg
 
     def recv_req(self, buf: bytearray) -> bool:
-        logging.info(f'recv_req: first byte modbus:{buf[0]} len:{len(buf)}')
+        # logging.info(f'recv_req: first byte modbus:{buf[0]} len:{len(buf)}')
         if not self.check_crc(buf):
+            self.err = 1
             logging.error('Modbus: CRC error')
             return False
         if buf[0] != self.INV_ADDR:
+            self.err = 2
             logging.info(f'Modbus: Wrong addr{buf[0]}')
             return False
         res = struct.unpack_from('>BHH', buf, 1)
         self.last_fcode = res[0]
         self.last_reg = res[1]
         self.last_len = res[2]
+        self.err = 0
         return True
 
     def recv_resp(self, info_db, buf: bytearray, node_id: str) -> \
             Generator[tuple[str, bool], None, None]:
-        logging.info(f'recv_resp: first byte modbus:{buf[0]} len:{len(buf)}')
+        # logging.info(f'recv_resp: first byte modbus:{buf[0]} len:{len(buf)}')
         if not self.check_crc(buf):
             logging.error('Modbus: CRC error')
+            self.err = 1
             return
         if buf[0] != self.INV_ADDR:
             logging.info(f'Modbus: Wrong addr {buf[0]}')
+            self.err = 2
             return
         if buf[1] != self.last_fcode:
             logging.info(f'Modbus: Wrong fcode {buf[1]} != {self.last_fcode}')
+            self.err = 3
             return
         elmlen = buf[2] >> 1
         if elmlen != self.last_len:
             logging.info(f'Modbus: len error {elmlen} != {self.last_len}')
+            self.err = 4
             return
+        self.err = 0
 
         for i in range(0, elmlen):
             val = struct.unpack_from('>H', buf, 3+2*i)
