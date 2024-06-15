dbff66affd56edadf39c4418176f9aa81ef8ab6f
Stefan Allius <stefan.allius@t-online.de>
2024-06-15 23:36:59 +0200

    add healthy check methods

--- app/src/gen3/connection_g3.py
+++ app/src/gen3/connection_g3.py
@@ -31,6 +31,10 @@ class ConnectionG3(AsyncStream, Talent):
     async def async_publ_mqtt(self) -> None:
         pass
 
+    def healthy(self) -> bool:
+        logger.debug('ConnectionG3 healthy()')
+        return AsyncStream.healthy(self)
+
     '''
     Our private methods
     '''
