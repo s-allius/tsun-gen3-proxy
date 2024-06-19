112b6b7a8a1722b981f5ad7b749b2d56e4a51a92
Stefan Allius <stefan.allius@t-online.de>
2024-06-19 21:55:21 +0200

    fix forwarding of MODBUS responses

--- app/src/gen3plus/solarman_v5.py
+++ app/src/gen3plus/solarman_v5.py
@@ -495,7 +495,8 @@ class SolarmanV5(Message):
 
         elif ftype == self.MB_RTU_CMD:
             if self.remoteStream.mb.recv_req(data[15:],
-                                             self.__forward_msg()):
+                                             self.remoteStream.
+                                             __forward_msg):
                 self.inc_counter('Modbus_Command')
             else:
                 self.inc_counter('Invalid_Msg_Format')
