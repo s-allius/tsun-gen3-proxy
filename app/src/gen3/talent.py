3eba999a2b223e3e3538615ea5b144b3b6bfcf7c
Stefan Allius <stefan.allius@t-online.de>
2024-10-22 00:06:31 +0200

    fix _send_modbus_cmd calls

--- app/src/gen3/talent.py
+++ app/src/gen3/talent.py
@@ -186,9 +186,11 @@ class Talent(Message):
 
         if 2 == (exp_cnt % 30):
             # logging.info("Regular Modbus Status request")
-            self._send_modbus_cmd(Modbus.READ_REGS, 0x2000, 96, logging.DEBUG)
+            self._send_modbus_cmd(Modbus.INV_ADDR, Modbus.READ_REGS, 0x2000,
+                                  96, logging.DEBUG)
         else:
-            self._send_modbus_cmd(Modbus.READ_REGS, 0x3000, 48, logging.DEBUG)
+            self._send_modbus_cmd(Modbus.INV_ADDR, Modbus.READ_REGS, 0x3000,
+                                  48, logging.DEBUG)
 
     def _init_new_client_conn(self) -> bool:
         contact_name = self.contact_name
