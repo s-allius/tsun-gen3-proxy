2f4c4b4a9cf5c098f522e697f02c0cb822c6915e
Stefan Allius <stefan.allius@t-online.de>
2024-11-25 23:19:16 +0100

    remove duplicates source files from repro

--- tsun-proxy-addon/rootfs/home/tsun-proxy/src/iter_registry.py
+++
@@ -1,9 +0,0 @@
-from abc import ABCMeta
-
-
-class AbstractIterMeta(ABCMeta):
-    def __iter__(cls):
-        for ref in cls._registry:
-            obj = ref()
-            if obj is not None:
-                yield obj
