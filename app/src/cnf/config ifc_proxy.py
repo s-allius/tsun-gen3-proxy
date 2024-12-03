d88944045959cfe0aebf6ec0bc1af1ae86938c05
Stefan Allius <stefan.allius@t-online.de>
2024-12-03 20:56:31 +0100

    split config class
    - use depency injection to get config

---
+++ app/src/cnf/config ifc_proxy.py
@@ -0,0 +1,34 @@
+'''Config module handles the proxy configuration in the config.toml file'''
+
+import shutil
+import tomllib
+import logging
+from config import ConfigIfc
+
+
+class ConfigIfcProxy(ConfigIfc):
+    def __init__(self):
+        try:
+            # make the default config transparaent by copying it
+            # in the config.example file
+            logging.info('Copy Default Config to config.example.toml')
+
+            shutil.copy2("default_config.toml",
+                         "config/config.example.toml")
+        except Exception:
+            pass
+
+    def get_config(self) -> dict:  # pragma: no cover
+        usr_config = {}
+
+        try:
+            with open("config/config.toml", "rb") as f:
+                usr_config = tomllib.load(f)
+        except Exception as error:
+            err = f'Config.read: {error}'
+            logging.error(err)
+            logging.info(
+                '\n  To create the missing config.toml file, '
+                'you can rename the template config.example.toml\n'
+                '  and customize it for your scenario.\n')
+        return usr_config
