b7b0d3755e86741af591ddb5087d9261b3f65f38
Stefan Allius <stefan.allius@t-online.de>
2025-03-16 12:20:55 +0100

    MS-3000: add integer test register

--- app/src/gen3/infos_g3.py
+++ app/src/gen3/infos_g3.py
@@ -47,21 +47,26 @@ class RegisterMap:
         0x000001f4: {'reg': Register.EVENT_FAULT},
         0x00000258: {'reg': Register.EVENT_BF1},
         0x000002bc: {'reg': Register.EVENT_BF2},
-
+        0x00000320: {'reg': Register.TEST_IVAL_1},
         0x000003e8: {'reg': Register.TEST_VAL_0},
-        0x0000044c: {'reg': Register.TEST_VAL_1},
+        0x0000044c: {'reg': Register.TEST_VAL_1},    # DC 1 Inpput Voltage *10
         0x000004b0: {'reg': Register.TEST_VAL_2},
         0x00000514: {'reg': Register.GRID_VOLTAGE},  # Grid Voltage
         0x00000578: {'reg': Register.GRID_CURRENT},  # Grid Current
         0x000005dc: {'reg': Register.TEST_VAL_3},
         0x00000640: {'reg': Register.GRID_FREQUENCY},
-        0x000007d0: {'reg': Register.TEST_VAL_4},
+        0x000006a4: {'reg': Register.TEST_IVAL_2},
+        0x00000708: {'reg': Register.TEST_IVAL_3},
+        0x0000076c: {'reg': Register.TEST_IVAL_4},
+        0x000007d0: {'reg': Register.TEST_VAL_4},    # DC 2 Input Voltage *10
         0x00000834: {'reg': Register.MAX_DESIGNED_POWER},
         0x00000898: {'reg': Register.OUTPUT_POWER},  # Grid Power
         0x000008fc: {'reg': Register.DAILY_GENERATION},  # Daily Generation
         0x00000960: {'reg': Register.TOTAL_GENERATION},  # Total Genration
+        0x000009c4: {'reg': Register.TEST_IVAL_5},
         0x00000a28: {'reg': Register.TEST_VAL_10},   # Isolationsimpedanz Rx
         0x00000a8c: {'reg': Register.TEST_VAL_11},   # Isolationsimpedanz Ry
+        0x00000af0: {'reg': Register.TEST_IVAL_6},
         0x000001324: {'reg': Register.PV1_VOLTAGE},  # PV1 Voltage
         0x000001388: {'reg': Register.PV1_CURRENT},  # PV1 Current
         0x0000013ec: {'reg': Register.PV1_POWER},    # PV1 Power
@@ -76,13 +81,16 @@ class RegisterMap:
         0x0000019c8: {'reg': Register.TEST_VAL_7},
         0x000001c20: {'reg': Register.TEST_VAL_14},
         0x000001c84: {'reg': Register.TEST_VAL_15},
-        0x000001ce8: {'reg': Register.TEST_VAL_16},
+        0x000001ce8: {'reg': Register.TEST_VAL_16},  # DC 1 Voltage
         0x000001d4c: {'reg': Register.TEST_VAL_17},
         0x000001db0: {'reg': Register.TEST_VAL_18},
+        0x000001e14: {'reg': Register.TEST_IVAL_8},
         0x000001e78: {'reg': Register.PV4_VOLTAGE},
         0x000001edc: {'reg': Register.PV4_CURRENT},
         0x000001f40: {'reg': Register.PV4_POWER},
         0x000001fa4: {'reg': Register.TEST_VAL_8},
+        0x0000020c9: {'reg': Register.TEST_IVAL_9},
+        0x0000020db: {'reg': Register.TEST_IVAL_10},
 
         0x000002134: {'reg': Register.PV5_VOLTAGE},
         0x000002198: {'reg': Register.PV5_CURRENT},
@@ -94,9 +102,11 @@ class RegisterMap:
         # 0x00000251c: {'reg': Register.TEST_VAL_14},
         0x000002774: {'reg': Register.TEST_VAL_24},
         0x0000027d8: {'reg': Register.TEST_VAL_25},
-        0x00000283c: {'reg': Register.TEST_VAL_26},
+        0x00000283c: {'reg': Register.TEST_VAL_26},  # DC 2 Voltage
         0x0000028a0: {'reg': Register.TEST_VAL_27},
         0x000002904: {'reg': Register.TEST_VAL_28},
+        0x000002968: {'reg': Register.TEST_IVAL_11},
+        0x0000029cc: {'reg': Register.TEST_IVAL_12},
     }
     map_01900001 = {
         0x0000000a: {'reg': Register.PRODUCT_NAME},
