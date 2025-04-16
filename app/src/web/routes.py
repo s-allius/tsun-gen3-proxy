e31a5a2f5a647cb911098b7631abec3a71a1eaa2
Stefan Allius <stefan.allius@t-online.de>
2025-04-17 00:14:48 +0200

    add w3.css dashboard

--- app/src/web/routes.py
+++ app/src/web/routes.py
@@ -5,5 +5,10 @@ web_routes = Blueprint('web_routes', __name__)
 
 
 @web_routes.route('/')
-async def hello():
+async def index():
     return await render_template('index.html')
+
+
+@web_routes.route('/page')
+async def empty():
+    return await render_template('empty.html')
