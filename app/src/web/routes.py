f33209bf8fc56fe1defee5220162fb2a619d8129
Stefan Allius <stefan.allius@t-online.de>
2025-04-17 20:33:17 +0200

    add favicon.ico

--- app/src/web/routes.py
+++ app/src/web/routes.py
@@ -1,5 +1,7 @@
 from quart import Blueprint
 from quart import render_template
+from quart import send_from_directory
+import os
 
 web_routes = Blueprint('web_routes', __name__)
 
@@ -12,3 +14,11 @@ async def index():
 @web_routes.route('/page')
 async def empty():
     return await render_template('empty.html')
+
+
+@web_routes.route('/favicon.ico')
+async def favicon():
+    return await send_from_directory(
+        os.path.join(web_routes.root_path, 'static/images'),
+        'logo.png',
+        mimetype='image/vnd.microsoft.icon')
