73d7593505104875380763ce1e98101b1e002f43
Stefan Allius <stefan.allius@t-online.de>
2025-05-03 20:19:20 +0200

    add mqtt info table

--- app/src/web/mqtt_table.py
+++ app/src/web/mqtt_table.py
@@ -6,42 +6,17 @@ from mqtt import Mqtt
 from . import web
 
 
-def _get_device_icon(client_mode: bool):
-    '''returns the icon for the device conntection'''
-    if client_mode:
-        return 'fa-download fa-rotate-180'
-
-    return 'fa-upload fa-rotate-180'
-
-
-def _get_cloud_icon(emu_mode: bool):
-    '''returns the icon for the cloud conntection'''
-    if emu_mode:
-        return 'fa-cloud-arrow-up-alt'
-
-    return 'fa-cloud'
-
-
 def _get_row(inv: InverterBase):
     '''build one row for the connection table'''
-    client_mode = inv.client_mode
+    entity_prfx = inv.entity_prfx
     inv_serial = inv.local.stream.inv_serial
-    icon1 = _get_device_icon(client_mode)
-    ip1, port1 = inv.addr
-    icon2 = ''
-    ip2 = '--'
-    port2 = '--'
-
-    if inv.remote.ifc:
-        ip2, port2 = inv.remote.ifc.r_addr
-        icon2 = _get_cloud_icon(client_mode)
+    node_id = inv.local.stream.node_id
+    sug_area = inv.local.stream.sug_area
 
     row = []
-    row.append(f'<i class="fa {icon1}"></i> {ip1}:{port1}')
-    row.append(f'<i class="fa {icon1}"></i> {ip1}')
     row.append(inv_serial)
-    row.append(f'<i class="fa {icon2}"></i> {ip2}:{port2}')
-    row.append(f'<i class="fa {icon2}"></i> {ip2}')
+    row.append(entity_prfx+node_id)
+    row.append(sug_area)
     return row
 
 
@@ -50,14 +25,14 @@ def get_table_data():
     table = {
         "headline": _('MQTT devices'),
         "col_classes": [
-            "w3-hide-small w3-hide-medium", "w3-hide-large",
             "",
-            "w3-hide-small w3-hide-medium", "w3-hide-large",
+            "",
+            "",
         ],
         "thead": [[
-            _('Device-IP:Port'), _('Device-IP'),
             _("Serial-No"),
-            _("Cloud-IP:Port"), _("Cloud-IP")
+            _('Node-ID'),
+            _('HA-Area'),
         ]],
         "tbody": []
     }
