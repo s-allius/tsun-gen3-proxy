c6822fd309e6256a1ee0ad6983bcf91ef2c1b800
Stefan Allius <stefan.allius@t-online.de>
2025-05-04 16:06:52 +0200

    add page for warnings and errors

---
+++ app/src/web/notes_table.py
@@ -0,0 +1,18 @@
+from quart import render_template
+from quart_babel import format_datetime
+
+from . import web
+from .log_handler import LogHandler
+
+
+@web.route('/notes-fetch')
+async def notes_fetch():
+    data = {
+        "update-time": format_datetime(format="medium"),
+    }
+
+    data["notes-list"] = await render_template(
+        'templ_notes_list.html.j2',
+        notes=LogHandler().get_buffer())
+
+    return data
