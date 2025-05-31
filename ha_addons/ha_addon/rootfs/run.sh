ffaef139a2e3848d3164bb8cfbe800ccb10199e1
Stefan Allius <stefan.allius@t-online.de>
2025-05-31 18:54:22 +0200

    handle missing MQTT addon
    - we have to check if the supervisor API and a
    MQTT broker add-on is installed. If not we assume
    the user has an external MQTT broker

--- ha_addons/ha_addon/rootfs/run.sh
+++ ha_addons/ha_addon/rootfs/run.sh
@@ -1,18 +1,28 @@
 #!/usr/bin/with-contenv bashio
 
-echo "Add-on environment started"
-
-echo "check for Home Assistant MQTT"
-MQTT_HOST=$(bashio::services mqtt "host")
-MQTT_PORT=$(bashio::services mqtt "port")
-MQTT_USER=$(bashio::services mqtt "username")
-MQTT_PASSWORD=$(bashio::services mqtt "password")
+echo "run.sh: info: Add-on environment started"
+bashio::cache.flush_all
+echo "run.sh: info: check for Home Assistant supervisor API"
+MQTT_HOST=""
+if bashio::supervisor.ping; then
+    echo "run.sh: info: check for Home Assistant MQTT"
+    if bashio::services mqtt; then
+        MQTT_HOST=$(bashio::services mqtt "host")
+        MQTT_PORT=$(bashio::services mqtt "port")
+        MQTT_USER=$(bashio::services mqtt "username")
+        MQTT_PASSWORD=$(bashio::services mqtt "password")
+    else
+        echo "run.sh: error: Home Assistant service MQTT not available!"
+    fi
+else
+    echo "run.sh: error: Home Assistant supervisor API not available!"
+fi
 
 # if a MQTT was/not found, drop a note
 if [ -z "$MQTT_HOST" ]; then
-    echo "MQTT not found"
+    echo "run.sh: info: MQTT configuration not found"
 else
-    echo "MQTT found"
+    echo "run.sh: info: MQTT found"
     export MQTT_HOST
     export MQTT_PORT
     export MQTT_USER
