11a7f28796bbb11a647d1f5e676e1c6bec2e1ff1
Stefan Allius <stefan.allius@t-online.de>
2026-08-30 13:30:05 +0200

    code cleanup

--- app/src/gen3plus/infos_g3p.py
+++ app/src/gen3plus/infos_g3p.py
@@ -347,6 +347,7 @@ class InfosG3P(Infos):
         self.set_db_def_value(Register.CHIP_TYPE, 'IGEN TECH')
 
     def __hide_topic(self, row: dict) -> bool:
+        '''Check if register should be hidden in HA'''
         if 'dep' in row:
             mode = row['dep']
             if self.client_mode:
@@ -355,7 +356,23 @@ class InfosG3P(Infos):
                 return mode != ProxyMode.SERVER
         return False
 
+    def __is_not_a_reg_def(self, idx: int) -> bool:
+        '''Check if index points to a Meta value'''
+        return 'calc' == idx or 'len' == idx or 'emu_support' == idx
+
+    def __update_val(self, node_id, source: str, info_id, result):
+        '''Update value in DB and make a well formed log trace'''
+        keys, level, unit, must_incr = self._key_obj(info_id)
+        if keys:
+            name, update = self.update_db(keys, must_incr, result)
+            yield keys[0], update
+            if update:
+                self.tracer.log(level, f'[{node_id}] {source}: {name}'
+                                       f' : {result}{unit}')
+
     def emu_supported(self, sensor: int) -> bool:
+        '''Check function returns a bool if the sensor_lists
+        supports Client Forwarding'''
         reg_map = RegisterSel.get(sensor)
         return reg_map['emu_support']
 
@@ -380,7 +397,7 @@ class InfosG3P(Infos):
             virt = {}
 
         for idx, row in chain(RegisterMap.map.items(), items, virt):
-            if 'calc' == idx or 'len' == idx or 'emu_support' == idx:
+            if self.__is_not_a_reg_def(idx):
                 continue
             info_id = row['reg']
             if self.__hide_topic(row):
@@ -399,7 +416,7 @@ class InfosG3P(Infos):
         buf: buffer of the sequence to parse'''
         reg_map = RegisterSel.get(sensor)
         for idx, row in reg_map.items():
-            if 'calc' == idx or 'len' == idx or 'emu_support' == idx:
+            if self.__is_not_a_reg_def(idx):
                 continue
             addr = idx & 0xffff
             ftype = (idx >> 16) & 0xff
@@ -430,21 +447,12 @@ class InfosG3P(Infos):
                 result = row['func'](self, row['params'])
                 yield from self.__update_val(node_id, "CALC", info_id, result)
 
-    def __update_val(self, node_id, source: str, info_id, result):
-        keys, level, unit, must_incr = self._key_obj(info_id)
-        if keys:
-            name, update = self.update_db(keys, must_incr, result)
-            yield keys[0], update
-            if update:
-                self.tracer.log(level, f'[{node_id}] {source}: {name}'
-                                       f' : {result}{unit}')
-
     def build(self, msg_type: int, rcv_ftype: int, sensor: int = 0):
         reg_map = RegisterSel.get(sensor)
         buf = bytearray(reg_map['len'])
         try:
             for idx, row in reg_map.items():
-                if 'calc' == idx or 'len' == idx or 'emu_support' == idx:
+                if self.__is_not_a_reg_def(idx):
                     continue
                 addr = idx & 0xffff
                 ftype = (idx >> 16) & 0xff
