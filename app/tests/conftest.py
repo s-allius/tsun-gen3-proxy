68e7e211bb31a561e3b8272c435f68a6cfd0d594
Stefan Allius <122395479+s-allius@users.noreply.github.com>
2026-09-04 23:32:46 +0200

    S allius/issue680 (#682)
    * call global constructor and destructor of the
    server app in pytest session scope

--- app/tests/conftest.py
+++ app/tests/conftest.py
@@ -1,13 +1,18 @@
 import pytest_asyncio
 import asyncio
 
+from server import startup_app, handle_shutdown
+
 pytest_plugins = ('pytest_asyncio',)
 
-@pytest_asyncio.fixture
+@pytest_asyncio.fixture(scope="session", loop_scope="session", autouse=True)
 async def my_loop():
+    await startup_app()
+
     event_loop = asyncio.get_running_loop()
     yield event_loop                     
 
+    await handle_shutdown()
     # Collect all tasks and cancel those that are not 'done'.  
     tasks = asyncio.all_tasks(event_loop)
     tasks = [t for t in tasks if not t.done()]
